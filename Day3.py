3e3b5833b2b39f1d241d6e4b4201b3e2383bac25
Likitha <85700674+LikithaM36@users.noreply.github.com>
2021-06-12 18:39:41 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,39 @@
+#AGE CALCULATOR
+print('Age calculator')
+yob = int(input('Enter your year of birth:'))
+cal = 2021-yob-1
+print('Now your age is',cal,'years')
+
+
+
+#Simple Calculator
+print('Simple Calculator')
+a = int(input('Enter the first num:'))
+b = int(input('Enter the second num:'))
+print('Addition:',a+b)
+print('Subtraction:',a-b)
+print('Multiplication:',a*b)
+print('Division:',a/b)
+print('a%b:',a%b)
+print('a//b:',a//b)
+print('a**b:',a**b)
+print('a<b: {}'.format(a<b))
+print(f'a>b: {a>b}')
+print(f'a<=b: {a<=b}')
+print(f'a>=b: {a>=b}')
+print(f'a==b: {a==b}')
+print(f'a!=b: {a!=b}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
